cb62998970af533ea83b8dcbdfc3f91b87ca8719
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2017-12-08 13:20:41 -0600

    finished chaining exercise

--- chaining.js
+++ chaining.js
@@ -6,9 +6,14 @@
 // Then output (either in the DOM or the console) the sum of all the resulting numbers.
 
 
-var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+let integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-integers.sort(function(a, b) {return b - a}).filter(function(x) {return x < 19;})
+// let newInteger = [];
+newInteger = integers.sort(function(a, b) {return b - a}).filter(function(x) {return x < 19;}).map(function(x) { return (x * 1.5) - 1}).reduce(function(a, b) { return a + b});
+// .forEach(function(item) {item * 1.5});
 
+console.log(newInteger);
 
-console.log(integers);
+// integers.forEach(function(item){ return item * 1.5 });
+// integers.forEach(function(item){ console.log(item * 1.5) });
+
